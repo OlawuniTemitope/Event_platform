6347d6cc09a1e5693802c5ad250ceef2dd03b5a7
Temitope Olawuni <olawunitemitope68@gmail.com>
2024-01-15 16:26:18 +0100

    first commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -10,7 +10,7 @@ if(cached.conn) return cached.conn;
 if(!MONGODB_URI) throw new Error('MONGODB_URI is missing')
 
 cached.Promise=cached.Promise  || mongoose.connect(MONGODB_URI,{
-    dbName:'Evently',
+    dbName:'evently',
     bufferCommands:false,
 })
 cached.conn =await cached.Promise
